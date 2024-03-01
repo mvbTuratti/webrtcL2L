839fda2a919424d19d1205c80f8faa6815e97593
Matheus <mvbturatti@gmail.com>
2024-02-29 22:57:40 -0300

    adds server to true

--- config/config.exs
+++ config/config.exs
@@ -13,6 +13,7 @@ config :webrtcL2L, WebrtcL2LWeb.Endpoint,
   render_errors: [view: WebrtcL2LWeb.ErrorView, accepts: ~w(html json), layout: false],
   pubsub_server: WebrtcL2L.PubSub,
   live_view: [signing_salt: "+RFoafRf"]
+  server: true
 
 # Configures the mailer
 #
