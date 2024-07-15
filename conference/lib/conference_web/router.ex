6e3f31d62beac4fd2a5a3e83fa764dff341631ad
Matheus <mvbturatti@gmail.com>
2024-07-15 20:18:04 -0300

    legacy code

--- conference/lib/conference_web/router.ex
+++ conference/lib/conference_web/router.ex
@@ -5,7 +5,7 @@ defmodule ConferenceWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_live_flash
-    plug :put_root_layout, html: {ConferenceWeb.Layouts, :root}
+    # plug :put_root_layout, html: {ConferenceWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
