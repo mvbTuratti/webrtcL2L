a5ae38e6a4c53999b4e555e71b5daebbec21cc91
Murilo Mascarin <mumascarin@yahoo.com.br>
2024-07-23 00:36:42 -0300

    outro wip

--- frontend/conference/src/components/conference/Lobby.tsx
+++ frontend/conference/src/components/conference/Lobby.tsx
@@ -1,9 +1,15 @@
+import { useLocation } from 'react-router-dom';
+
 function Lobby() {
+    const location = useLocation();
+    const { inputValue } = location.state || { inputValue: '' };
+
     return (
         <>
-            Teste
+            <h1>Lobby</h1>
+            <p>Valor passado: {inputValue}</p>
         </>
     );
 }
- 
+
 export default Lobby;
--- frontend/conference/src/components/home/Home.tsx
+++ frontend/conference/src/components/home/Home.tsx
@@ -1,16 +1,17 @@
-import { redirect } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
-
 function Home() {
-    const [state, setState] = useState("")
-    const redirectToLobby = () => {
-        return redirect(`/lobby/`);
+    const [state, setState] = useState("");
+    const navigate = useNavigate();
+
+    const redirectToLobby = (event) => {
+        event.preventDefault();
+        navigate(`/lobby/${state}`, { state: { inputValue: state } });
     }
 
     return (
         <>
-            
             <form className="max-w-md mx-auto" onSubmit={redirectToLobby}>   
                 <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                 <div className="relative">
@@ -19,11 +20,25 @@ function Home() {
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                         </svg>
                     </div>
-                    <input type="search" id="default-search" value={state} onChange={(key: any) => setState(key.target.value)} className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Entre em uma sala..." required />
-                    <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Entrar</button>
+                    <input 
+                        type="search" 
+                        id="default-search" 
+                        value={state} 
+                        onChange={(key: any) => setState(key.target.value)} 
+                        className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                        placeholder="Entre em uma sala..." 
+                        required 
+                    />
+                    <button 
+                        type="submit" 
+                        className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                        Entrar
+                    </button>
                 </div>
             </form>
         </>
     );
 }
+
 export default Home;
