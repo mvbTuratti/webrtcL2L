898461f8407938311c3974f6d56f0cb140f0ea88
Murilo Mascarin <mumascarin@gmail.com>
2024-07-28 21:06:59 -0300

    wip

--- frontend/conference/src/components/conference/Lobby.tsx
+++ frontend/conference/src/components/conference/Lobby.tsx
@@ -1,13 +1,16 @@
 import { useLocation } from 'react-router-dom';
+import toggleMachine from './state-machine';
 
 function Lobby() {
     const location = useLocation();
     const { inputValue } = location.state || { inputValue: '' };
-
+    const machine = toggleMachine;
     return (
         <>
             <h1>Lobby</h1>
             <p>Valor passado: {inputValue}</p>
+    
+
         </>
     );
 }
