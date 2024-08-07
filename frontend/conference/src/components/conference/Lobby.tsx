60bc749de6e21021f073363c79fdc0b6fbc84f4b
Matheus <mvbturatti@gmail.com>
2024-08-07 20:34:01 -0300

    wip

--- frontend/conference/src/components/conference/Lobby.tsx
+++ frontend/conference/src/components/conference/Lobby.tsx
@@ -1,16 +1,15 @@
 import { useParams } from 'react-router-dom';
-import toggleMachine from './state-machine';
+import Fetcher from './state-machine.jsx';
 
 function Lobby() {
     const location = useParams();
     const inputValue = location.roomId;
-    const machine = toggleMachine;
 
     return (
         <>
             <h1>Lobby</h1>
             <p>Valor passado: {inputValue}</p>
-    
+            <Fetcher onResolve={console.log}></Fetcher>
 
         </>
     );
